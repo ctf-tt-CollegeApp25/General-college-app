43d2802173553c1633b95dcbfbdf02097c4ecfe9
Gougl11 <gogulsmart11@gmail.com>
2024-12-27 13:48:01 +0530

    Gogul : Added components folder

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Slot } from 'expo-router';
 
+import Demo from './components/demo';
 import "../global.css"
 
 
@@ -10,6 +11,7 @@ export default function App() {
         <View className="flex flex-col justify-center items-center h-screen">
             <Text className="text-red-500 text-[30px]">General college app</Text>
             <StatusBar style="auto" />
+            <Demo/>
         </View>
     );
 }
---
+++ app/components/demo.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <View>
+      <Text className="text-green-600">This is demo file</Text>
+    </View>
+  )
+}
+
+export default Demo
+
+const styles = StyleSheet.create({})
