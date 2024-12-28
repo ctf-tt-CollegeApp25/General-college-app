9fa9b9a26bdf200a7704175f110c6e80e6d2c36c
Suhaib Sharieff <sharieffsuhaib@gmail.com>
2024-12-28 20:48:57 +0530

    Suhaib - Complete tabs

---
+++ app/(tabs)/addItem.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+const AddItemPage = () => {
+  return (
+    <View>
+      <Text className='text-2xl text-red-800'>AddItemPage</Text> //css working
+    </View>
+  )
+}
+
+export default AddItemPage
---
+++ components/LostAndFound.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LostAndFound = () => {
+  return (
+    <View>
+      <Text>LostAndFound</Text>
+    </View>
+  )
+}
+
+export default LostAndFound
