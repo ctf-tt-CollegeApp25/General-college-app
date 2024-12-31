93566743dff3b15b20e6344bd639f6ffe18dbf9b
Gougl11 <gogulsmart11@gmail.com>
2024-12-31 12:23:42 +0530

    Gogul : Modified tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{jsx, js, tsx, ts}", "./app/components/**/*.{jsx, js, tsx, ts}"],
+  content: ["./app/**/*.{jsx, js, tsx, ts}", "./components/**/*.{jsx, js, tsx, ts}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
